cef5d53b86063a87a6df61a4185ee048a8b9809c
himups9 <himups9@gmail.com>
2026-08-29 22:46:08 +0545

    asios update

--- frontend/src/features/api/axios.js
+++ frontend/src/features/api/axios.js
@@ -1,8 +1,14 @@
 import axios from "axios";
 
 const API_BASE_URL =
-    import.meta.env.VITE_API_URL ||
-    "http://127.0.0.1:5001/api";
+    import.meta.env.VITE_API_URL; //||
+    //"http://127.0.0.1:5001/api";
+
+    if (!API_BASE_URL) {
+        throw new Error(
+            "VITE_API_URL is not configured."
+        );
+    }
 
 const api = axios.create({
     baseURL: API_BASE_URL,
