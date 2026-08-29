2e75d5b7456fc8e67d6c530faf19e21d2070012e
himups9 <himups9@gmail.com>
2026-08-29 14:01:37 +0545

    vite.config modified

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,22 +1,29 @@
 // frontend/vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 
   optimizeDeps: {
-    exclude: [
-      "lucide-react",
-    ],
+    exclude: ["lucide-react"],
   },
+
   server: {
     proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8000',
+      "/api": {
+        target: "http://127.0.0.1:5001",
         changeOrigin: true,
       },
     },
   },
-})
+
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+  },
+});
